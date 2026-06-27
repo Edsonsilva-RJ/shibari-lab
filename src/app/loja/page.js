d072787612746864ab0e7fa345b703cb5d247acb
Edson Silva <edson23jpa@gmail.com>
2026-06-26 21:14:31 -0300

    Atualização loja

--- src/app/loja/page.js
+++ src/app/loja/page.js
@@ -11,31 +11,77 @@ export default function LojaPage() {
   const produtos = [
     {
       id: 1,
-      titulo: "Corda de Juta Tratada - 8m",
-      categoria: "Equipamentos",
-      imagem: "", // Quando tiver o ID real, ele usa este...
-      descricao: "Corda de juta natural de alta qualidade (6mm), lavada, fervida, amaciada com óleo mineral e cera de abelha. Pronta para uso em suspensões ou amarras de chão.",
-      preco: "R$ 85,00",
+      titulo: "Corda de Juta Tratada - 8m / 5.5mm",
+      categoria: "Equipamento",
+      imagem: "", // Deixei vaio para testar ouso da imagem generica
+      descricao: "Corda de juta natural de alta qualidade, tratada com óleo de jojoba e cera de abelha. Pronta para uso em  amarras de chão ou suspensões.",
+      preco: "R$ 75,00",
       linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+na+Corda+de+Juta+Tratada+de+8m"
     },
     {
       id: 2,
-      titulo: "Print Fotográfico - Ensaio Geométrico",
-      categoria: "Arte & Fotografia",
+      titulo: "Cera hidratante para cordas- 50ml",
+      categoria: "Item",
       imagem: "", // Deixando vazio para testar o funcionamento da imagem genérica
-      descricao: "Impressão Fine Art em papel fosco de alta gramatura (Tamanho A3). Fotografia autoral analógica focada nas lines, sombras e contrastes das amarras.",
-      preco: "R$ 120,00",
-      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+no+Print+Fotogr%C3%A1fico+do+Ensaio+Geom%C3%A9trico"
+      descricao: "Cera hidratante ideal para cordas de fibra natural, feita com ingredientes pensados para mantém a corda hidratada, flexível e resistente.",
+      preco: "R$ 34,20",
+      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+na+cera+hidratante+para+cordas"
     },
     {
       id: 3,
-      titulo: "Kit Principiante: 2 Cordas + Canivete ESD",
-      categoria: "Kits",
+      titulo: "Kit iniciante: 2 Cordas + Tesoura de ponta romba",
+      categoria: "Kit",
       imagem: "",
-      descricao: "Combo ideal para quem está iniciando: duas cordas de juta tratadas de 8m e um canivete de segurança (bico redondo) para cortes rápidos de emergência.",
-      preco: "R$ 190,00",
-      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+no+Kit+Principiante"
+      descricao: "Combo ideal para quem está iniciando: duas cordas de juta tratadas de 8m e uma tesoura de ponta romba (ponta redonda) para cortes seguros e rápidos nos momentos de emergência.",
+      preco: "R$ 165,00",
+      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+no+Kit+iniciante"
+    },
+    {
+      id: 4,
+      titulo: "Corda de Juta colorida Tratada - 8m / 5.5mm",
+      categoria: "Equipamento",
+      imagem: "", // Deixei vaio para testar o uso da imagem generica
+      descricao: "Corda de juta tingida de alta qualidade, tratada com óleo de jojoba e cera de abelha. Pronta para uso em  amarras de chão ou suspensões.",
+      preco: "R$ 115,00",
+      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+na+Corda+de+Juta+Colorida+Tratada+de+8m"
+    },
+    {
+      id: 5,
+      titulo: "Cera hidratante premium para cordas- 50ml",
+      categoria: "Item",
+      imagem: "", // Deixando vazio para testar o funcionamento da imagem genérica
+      descricao: "Cera hidratante premium ideal para cordas de fibra natural, feita com ingredientes 100% organicos pensados para manter a corda hidratada, flexível e resistente.",
+      preco: "R$ 45,25",
+      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+na+cera+hidratante+premium+para+cordas"
+    },
+    {
+      id: 6,
+      titulo: "Kit iniciante II: 2 Cordas + Tesoura de ponta romba + Cera hidratante 50 Gr",
+      categoria: "Kit",
+      imagem: "",
+      descricao: "Combo ideal para quem está iniciando: duas cordas de juta tratadas de 8m, cera hidratante 50Gr e uma tesoura de ponta romba (ponta redonda) para cortes seguros e rápidos nos momentos de emergência.",
+      preco: "R$ 195,00",
+      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+no+Kit+iniciante+II"
+    },
+    {
+      id: 7,
+      titulo: "Tesoura de ponta romba",
+      categoria: "Equipamento",
+      imagem: "",
+      descricao: "tesoura de ponta romba (ponta redonda) para cortes seguros e rápidos nos momentos de emergência.",
+      preco: "R$ 25,00",
+      linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+na+Tesoura+de+ponta+romba"
     }
+
+      // ,{
+    //   id: 2,
+    //   titulo: "Print Fotográfico - Ensaio Geométrico",
+    //   categoria: "Arte & Fotografia",
+    //   imagem: "", // Deixando vazio para testar o funcionamento da imagem genérica
+    //   descricao: "Impressão Fine Art em papel fosco de alta gramatura (Tamanho A3). Fotografia autoral analógica focada nas lines, sombras e contrastes das amarras.",
+    //   preco: "R$ 120,00",
+    //   linkWhats: "https://wa.me/5521994430177?text=Ol%C3%A1%21+Tenho+interesse+no+Print+Fotogr%C3%A1fico+do+Ensaio+Geom%C3%A9trico"
+    // }
   ];
 
   return (
